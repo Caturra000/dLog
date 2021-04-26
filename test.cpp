9e9d8d7d6ec589ce15f043b1c95e0e8afb1b2675
Caturra <wasdjkl233@gmail.com>
2021-04-27 02:07:37 +0800

    test version 0.3

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ namespace dlog {
 // shared mutable variables
 // protected by namespace
 
-constexpr size_t DLOG_BUFSIZE = 1<<18;
+constexpr size_t DLOG_BUFSIZE = 233;
 char buf[2][DLOG_BUFSIZE];
 int ridx = 0, rcur = 0;
 int widx = 1, wcur = 0;
@@ -22,78 +22,39 @@ struct LogBase {
 
     template <typename T, typename ...Ts>
     static void d(T &&msg, Ts &&...others);
-};
-// 确定该用哪个buffer，不用从每个Stream.from()接口都传入固定的buf
-// 并且提供阻塞（当r/w失衡时）
-struct Scheduler {
 
-    static std::mutex rmtx, wmtx;
-    static std::condition_variable rcond, wcond;
+    template <typename T>
+    static void i(T &&msg);
 
-    // brief: start a write-log thread
-    // usage: Scheduler::start().detach()
-    // TODO stop it manually
-    static std::thread start() {
-        return std::thread {[&] {
-            while(true) {
-                {
-                    std::unique_lock<std::mutex> lk{wmtx};
-                    wcond.wait(lk, [&] { return wcur != 0; });
-                    // write to file
-                    // ...
-                    // 是否需要主动swap？
-                    // try to lock rmtx // avoid deadlock
-                    // widx ^= 1; ridx ^= 1; ...
-                    wcur = 0;
-                }
-                rcond.notify_one();
-                // if(stop) break;
-            }
-        }};
-    }
+    template <typename T, typename ...Ts>
+    static void i(T &&msg, Ts &&...others);
 
-    // return a current read buffer for user
-    static char* current() {
-        return buf[ridx];
-    }
+    template <typename ...Ts>
+    static void info(Ts &&...msg);
 
-    // called by looper thread
-    static void log(/*T msg&, */size_t length) {
-        // parse T locally without lock
-        // ...
-        char tmp[1<<10 /*StreamTraitis<T>::size*/];
-        // Stream::parse(tmp, msg, length);
-
-        {
-            std::lock_guard<std::mutex> lk{rmtx};
-            // auto rbuf = current();
-            if(length > sizeof(buf[0]) - rcur + 2 /*' ' or \n */ /*|| timeout*/) {
-                {
-                    std::unique_lock<std::mutex> _{wmtx};
-                    rcond.wait(_, [&] { return wcur == 0; });
-                    ridx ^= 1; widx ^= 1;
-                    wcur = rcur; rcur = 0;
-                }
-                wcond.notify_one();
-            }
-            
-            auto rbuf = current();
-            // write to rbuf
-            for(int i = 0; i < length; ++i) rbuf[rcur++] = tmp[i];
-            // if(endOfLine) rbuf[rcur++] = '\n';
-        }
-        wcond.notify_one();
-    }
-    
 };
 
-template <size_t N = 1024> struct StreamTraitisBase { static constexpr size_t size = N; };
-template <typename T> struct StreamTraits: public StreamTraitisBase<> {};
-template <> struct StreamTraits<int>: public StreamTraitisBase<12> {}; // std::log10(INT_MAX)+1 + 1
-template <> struct StreamTraits<long>: public StreamTraitisBase<21> {};
-template <> struct StreamTraits<double>: public StreamTraitisBase<42> {};
-template <> struct StreamTraits<char>: public StreamTraitisBase<3> {};
-template <size_t N> struct StreamTraits<const char(&)[N]>: public StreamTraitisBase<N> {};
+
+template <size_t N = 1024> struct StreamTraitsBase { static constexpr size_t size = N; };
+template <typename T> struct StreamTraits: public StreamTraitsBase<> {};
+template <> struct StreamTraits<int>: public StreamTraitsBase<12> {}; // std::log10(INT_MAX)+1 + 1
+template <> struct StreamTraits<long>: public StreamTraitsBase<21> {};
+template <> struct StreamTraits<long long>: public StreamTraitsBase<21> {};
+template <> struct StreamTraits<double>: public StreamTraitsBase<42> {};
+template <> struct StreamTraits<char>: public StreamTraitsBase<2> {};
+template <size_t N> struct StreamTraits<const char(&)[N]>: public StreamTraitsBase<N> {};
+template <size_t N> struct StreamTraits<const char[N]>: public StreamTraitsBase<N> {};
+// use rvalue and perfect forward to support const char[]
+template <typename T> inline constexpr size_t bufcnt(T &&) { return StreamTraits<T>::size; }
+template <typename T, typename ...Ts> inline constexpr size_t bufcnt(T &&, Ts &&...others) {
+    return StreamTraits<T>::size + bufcnt(std::forward<Ts>(others)...);
+}
+
+template <typename T> inline constexpr size_t strcnt(T&&) { return 0; }
+template <size_t N> inline constexpr size_t strcnt(const char (&)[N]) { return 1; }
+template <typename T, typename ...Ts> inline constexpr size_t strcnt(T &&t, Ts &&...ts) {
+    return strcnt(std::forward<T>(t)) + strcnt(std::forward<Ts>(ts)...); 
+}
 
 
 template <typename T>
@@ -136,6 +97,85 @@ struct Stream {
         ExtraStream<T>::parse(whatever);
     }
 };
+
+std::mutex rmtx, wmtx;
+std::condition_variable rcond, wcond;
+using namespace std::literals::chrono_literals;
+using Timestamp = std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>;
+std::once_flag cflag;
+Timestamp checkpoint;
+
+// 确定该用哪个buffer，不用从每个Stream.from()接口都传入固定的buf
+// 并且提供阻塞（当r/w失衡时）
+struct Scheduler {
+
+    
+
+    // brief: start a write-log thread
+    // usage: Scheduler::start().detach()
+    // TODO stop it manually
+    static std::thread start() {
+        std::call_once(cflag, [] { checkpoint = std::chrono::system_clock::now(); });
+        return std::thread {[]() mutable {
+            while(true) {
+                {
+                    std::unique_lock<std::mutex> lk{wmtx};
+                    wcond.wait(lk, [] { return wcur != 0; });
+                    // write to file
+                    // fake
+                    for(int i = 0; i < wcur; ++i) std::cerr << buf[widx][i];
+                    // ...
+                    // 是否需要主动swap？
+                    // try to lock rmtx // avoid deadlock
+                    // widx ^= 1; ridx ^= 1; ...
+                    wcur = 0;
+                }
+                rcond.notify_one();
+                // if(stop) break;
+            }
+        }};
+    }
+
+    // return a current read buffer for user
+    static char* current() {
+        return buf[ridx];
+    }
+
+    // called by looper thread
+    template <typename T>
+    static void log(const T &msg,size_t length, char ch = ' ') {
+        // parse T locally without lock
+        // ...
+        char tmp[StreamTraits<T>::size];
+        Stream::parse(tmp, msg, length);
+
+        {
+            std::lock_guard<std::mutex> lk{rmtx};
+            // auto rbuf = current();
+            if(rcur + length > sizeof(buf[0]) + 2 /*' ' or \n */ || (std::chrono::system_clock::now() - checkpoint) > 500ms) {
+                {
+                    std::unique_lock<std::mutex> _{wmtx};
+                    rcond.wait(_, [] { return wcur == 0; });
+                    ridx ^= 1; widx ^= 1;
+                    wcur = rcur; rcur = 0;
+                }
+                wcond.notify_one();
+                checkpoint = std::chrono::system_clock::now();
+            }
+            
+            auto rbuf = current();
+            // write to rbuf
+            for(int i = 0; i < length; ++i) rbuf[rcur++] = tmp[i];
+            if(ch) rbuf[rcur++] = ch;
+        }
+        wcond.notify_one();
+    }
+    
+};
+
+
+
+
 using Log = LogBase;
 
 
@@ -156,6 +196,88 @@ inline void LogBase::d(T &&msg, Ts &&...others) {
     rcur++;
     d(std::forward<Ts>(others)...);
 }
+
+template <typename T>
+inline void LogBase::i(T &&msg) {
+    Scheduler::log(msg, Stream::parseLength(msg), '\n');
+}
+
+template <typename T, typename ...Ts>
+inline void LogBase::i(T &&msg, Ts &&...others) {
+    Scheduler::log(msg, Stream::parseLength(msg));
+    i(std::forward<Ts>(others)...);
+}
+
+
+
+struct Iov {
+    const char *base;
+    size_t len;
+};
+
+struct Wrap {
+    char *tmp;
+    const char **tmpref;
+    size_t cur;
+    size_t refcur;
+    // Iov *ioves;
+};
+
+template <typename T>
+inline void processDispatch(Wrap &wrap, T &&msg, char ch) {
+    size_t length = Stream::parseLength(msg);
+    char *buf = wrap.tmp + wrap.cur;
+    Stream::parse(buf, msg, length);
+    if(ch) Stream::parse(buf + length, ch, 1);
+    wrap.cur += length + (ch != 0);
+}
+
+// template <size_t N>
+// inline void processDispatch(Wrap &wrap, const char (&msg)[N], char ch) {
+//     size_t length = Stream::parseLength(msg);
+//     char *buf = wrap.tmp + wrap.cur;
+    
+//     iov
+
+//     if(ch) Stream::parse(buf + length, ch, 1);
+//     wrap.cur += length + (ch != 0);
+// }
+
+
+template <typename T>
+inline void process(Wrap &wrap, T &&msg) {
+    processDispatch(wrap, msg, '\n');
+}
+
+template <typename T, typename ...Ts>
+inline void process(Wrap &wrap, T &&msg, Ts &&...others) {
+    processDispatch(wrap, msg, ' ');
+    process(wrap, std::forward<Ts>(others)...);
+}
+
+
+
+
+
+
+template <typename ...Ts>
+inline void LogBase::info(Ts &&...msg) {
+    char tmp[bufcnt(std::forward<Ts>(msg)...)];
+    const char *tmpref[strcnt(std::forward<Ts>(msg)...)]; // an array stores char_ptr
+    // Iov ioves[sizeof...(msg)];
+    // TODO use std::array
+    size_t cur = 0, refcur = 0;
+    Wrap wrap {
+        .tmp = tmp,
+        .tmpref = tmpref, // unused
+        .cur = cur,
+        .refcur = refcur, // unused
+        // .ioves = ioves
+    };
+    process(wrap, std::forward<Ts>(msg)...);
+}
+
+
 
 
 
@@ -280,9 +402,13 @@ inline size_t Stream::parseLength(double val) {
 
 int main() {
     using namespace dlog;
-    Log::d(1, -223ll, "789", std::string("12.345"), 78.1, 78.0, 78.123445, 78.999, 0.0001);
-    Log::d(1, 3, 4, 6, -3, 12.345, "are you ok?", -34.23);
-    Log::d(-0.102);
-    std::cout << buf[ridx];
+    //auto t = Scheduler::start();
+    Log::info(1, -223ll, "789", std::string("12.345"), 78.1, 78.0, 78.123445, 78.999, 0.0001);
+    Log::info(1, 3, 4, 6, -3, 12.345, "are you ok?", -34.23);
+    Log::info(-0.102);
+    Log::info(123);
+    Log::info(1, -223ll);
+    
+    //t.join();
     return 0;
 }
