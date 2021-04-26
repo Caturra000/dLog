2e3add78e9142d72bfe3f3eaf50907c19e9fcb65
Caturra <wasdjkl233@gmail.com>
2021-04-26 17:47:24 +0800

    test version 0.2.2

--- test.cpp
+++ test.cpp
@@ -18,76 +18,10 @@ struct LogBase {
     // Stream
     
     template <typename T>
-    static void d(T &&msg) {
-        out(msg, buf[ridx]);
-        out("\n", buf[ridx]);
-    }
+    static void d(T &&msg);
+
     template <typename T, typename ...Ts>
-    static void d(T &&msg, Ts &&...others) {
-        out(msg, buf[ridx]);
-        out(" ", buf[ridx]);
-        d(std::forward<Ts>(others)...);
-    }
-    template <typename T,
-    typename = std::enable_if_t<std::is_integral<T>::value>>
-    static void out(T content, char *buf) {
-        if(content < 0) {
-            buf[rcur++] = '-';
-            content = -content;
-        } else if(content == 0) {
-            buf[rcur++] = '0';
-            return;
-        }
-        // int begin = rcur;
-        char buf2[64], i{};
-        while(content) {
-            char v = (content%10)+'0';
-            //buf[rcur++] = v;
-            buf2[i++] = v;
-            content /= 10;
-        }
-        // int end = rcur-1;
-        // while(begin < end) {
-        //     std::swap(buf[begin++], buf[end--]);
-        // }
-        --i;
-        while(~i) buf[rcur++] = buf2[i--];
-    }
-    static void out(const char ch, char *buf) {
-        buf[rcur++] = ch;
-    }
-    template <size_t N>
-    static void out(const char (&from)[N], char *buf) {
-        out(from, N-1, buf);
-    }
-    static void out(const std::string &str, char *buf) {
-        out(str.c_str(), str.length(), buf);
-    }
-    static void out(const char *from, size_t N, char *buf) {
-        std::memcpy(buf + rcur, from, sizeof(N));
-        rcur += N;
-    }
-    static void out(double content, char *buf) {
-        if(content < 0) {
-            buf[rcur++] = '-';
-            content = -content;
-        }
-        long i = content;
-        out(i, buf);
-        content -= i;
-        constexpr double EPS = 1e-12;
-        buf[rcur++] = '.';
-        int limit = 5, v;
-        do {
-            content *= 10;
-            v = content;
-            buf[rcur++] = v | 48;
-            content -= v;
-        } while(content > EPS && limit--);
-    }
-    
-    static void assign() {
-    }
+    static void d(T &&msg, Ts &&...others);
 };
 // 确定该用哪个buffer，不用从每个Stream.from()接口都传入固定的buf
 // 并且提供阻塞（当r/w失衡时）
@@ -168,94 +102,149 @@ struct ExtraStream;
 struct Stream {
     template <typename T,
     typename = std::enable_if_t<std::is_integral<T>::value>>
-    static void parse(char *buf, T msg, size_t length) {
-        int cur = length-1;
-        if(msg < 0) {
-            buf[0] = '-';
-            msg = -msg;
-        } else if(msg == 0) {
-            buf[0] = '0';
-            return;
-        }
-        while(msg) {
-            char v = (msg % 10) + '0';
-            buf[cur--] = v;
-            msg /= 10;
-        }
-    }
+    static void parse(char *buf, T msg, size_t length);
 
-    static void parse(char *buf, const char *msg, size_t length) {
-        std::memcpy(buf, msg, length);
-    }
+    static void parse(char *buf, const char *msg, size_t length);
 
     template <size_t N>
-    static void parse(char *buf, const char (&msg)[N], size_t length) {
-        // TODO no copy
-        parse(buf, (char*)msg, length);
-    }
+    static void parse(char *buf, const char (&msg)[N], size_t length);
 
-    static void parse(char *buf, double msg, size_t length) {
-        long ival = msg;
-        size_t ilen = parseLength(ival);
-        parse(buf, ival, ilen);
-        buf += ilen;
-        msg -= ival;
-        length -= ilen + 1;
-        int cur = 0;
-        buf[cur++] = '.';
-        constexpr double EPS = 1e-12;
-        int limit = 5, v;
-        do {
-            msg *= 10;
-            v = msg;
-            buf[cur++] = v | 48;
-            msg -= v;
-        } while(msg > EPS && limit--);
-    }
+    static void parse(char *buf, double msg, size_t length);
 
-    static void parse(char *buf, char msg, ...) {
-        buf[0] = msg;
-    }
+    static void parse(char *buf, char msg, size_t length);
 
     template <typename T,
     typename = std::enable_if_t<std::is_integral<T>::value>>
-    static size_t parseLength(T val) {
-        if(val >= 0) {
-            if(val == 0)
-                return 0;
-            if(val >= 1 && val <= 9)
-                return 1;
-            if(val >= 10 && val <= 99)
-                return 2;
-            if(val >= 100 && val <= 999)
-                return 3;
-            if(val >= 1000 && val <= 9999)
-                return 4;
-            if(val >= 10000 && val <= 99999)
-                return 5;
-            if(val >= 100000 && val <= 999999)
-                return 6;
-            if(val >= 1000000 && val <= 9999999)
-                return 7;
-            if(val >= 10000000 && val <= 99999999)
-                return 8;
-            if(val >= 100000000 && val <= 999999999)
-                return 9;
-            return 10; // TODO long
-        }
-        return 1 + parseLength(-val);
-    }
+    static size_t parseLength(T val);
 
     template <size_t N>
-    static size_t parseLength(const char (&str)[N]) {
-        return N-1;
-    }
+    static size_t parseLength(const char (&str)[N]) { return N-1; }
 
-    static size_t parseLength(const char *str) {
-        return strlen(str);
-    }
+    static size_t parseLength(const char *str) { return strlen(str); }
 
-    static size_t parseLength(double val) {
+    static size_t parseLength(double val);
+
+    static constexpr size_t parseLength(char ch) { return 1; }
+
+    template <typename T>
+    static void parse(T &whatever) {
+        // 这里通过ExtraStream<>来扩容，且不用为基础类也分出一堆的Stream struct
+        ExtraStream<T>::parse(whatever);
+    }
+};
+using Log = LogBase;
+
+
+// hard code
+template <typename T>
+inline void LogBase::d(T &&msg) {
+    Stream::parse(Scheduler::current() + rcur, msg, Stream::parseLength(msg));
+    rcur += Stream::parseLength(msg);
+    Stream::parse(Scheduler::current() + rcur, '\n', Stream::parseLength('\n'));
+    rcur += 1;
+}
+
+template <typename T, typename ...Ts>
+inline void LogBase::d(T &&msg, Ts &&...others) {
+    Stream::parse(Scheduler::current() + rcur, msg, Stream::parseLength(msg));
+    rcur += Stream::parseLength(msg);
+    Stream::parse(Scheduler::current() + rcur, ' ', Stream::parseLength(' '));
+    rcur++;
+    d(std::forward<Ts>(others)...);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+template <typename T,typename>
+inline void Stream::parse(char *buf, T msg, size_t length) {
+    int cur = length-1;
+    if(msg < 0) {
+        buf[0] = '-';
+        msg = -msg;
+    } else if(msg == 0) {
+        buf[0] = '0';
+        return;
+    }
+    while(msg) {
+        char v = (msg % 10) + '0';
+        buf[cur--] = v;
+        msg /= 10;
+    }
+}
+
+inline void Stream::parse(char *buf, const char *msg, size_t length) {
+    std::memcpy(buf, msg, length);
+}
+
+template <size_t N>
+inline void Stream::parse(char *buf, const char (&msg)[N], size_t length) {
+    // TODO no copy
+    parse(buf, (char*)msg, length);
+}
+
+
+inline void Stream::parse(char *buf, double msg, size_t length) {
+    long ival = msg;
+    size_t ilen = parseLength(ival);
+    parse(buf, ival, ilen);
+    buf += ilen;
+    msg -= ival;
+    length -= ilen + 1;
+    int cur = 0;
+    buf[cur++] = '.';
+    constexpr double EPS = 1e-12;
+    int limit = 5, v;
+    do {
+        msg *= 10;
+        v = msg;
+        buf[cur++] = v | 48;
+        msg -= v;
+    } while(msg > EPS && limit--);
+}
+
+inline void Stream::parse(char *buf, char msg, size_t) {
+    buf[0] = msg;
+}
+
+
+template <typename T,typename>
+inline size_t Stream::parseLength(T val) {
+    if(val >= 0) {
+        if(val == 0)
+            return 0;
+        if(val >= 1 && val <= 9)
+            return 1;
+        if(val >= 10 && val <= 99)
+            return 2;
+        if(val >= 100 && val <= 999)
+            return 3;
+        if(val >= 1000 && val <= 9999)
+            return 4;
+        if(val >= 10000 && val <= 99999)
+            return 5;
+        if(val >= 100000 && val <= 999999)
+            return 6;
+        if(val >= 1000000 && val <= 9999999)
+            return 7;
+        if(val >= 10000000 && val <= 99999999)
+            return 8;
+        if(val >= 100000000 && val <= 999999999)
+            return 9;
+        return 10; // TODO long
+    }
+    return 1 + parseLength(-val);
+}
+
+inline size_t Stream::parseLength(double val) {
         long ival = val;
         size_t len = parseLength(ival) + 1; // '.'
         val -= ival;
@@ -270,26 +259,13 @@ struct Stream {
         return len;
     }
 
-    static constexpr size_t parseLength(char ch) {
-        return 1;
-    }
-
-    template <typename T>
-    static void parse(T &whatever) {
-        // 这里通过ExtraStream<>来扩容，且不用为基础类也分出一堆的Stream struct
-        ExtraStream<T>::parse(whatever);
-    }
-};
-using Log = LogBase;
-
-
-
 } // dlog
 
 
 int main() {
     using namespace dlog;
-    Log::d(1, -223ll, "789", std::string("12.345"), 78.1, 78.0, 78.123445, 78.999, 0.0001);
+    //Log::d(1, -223ll, "789", std::string("12.345"), 78.1, 78.0, 78.123445, 78.999, 0.0001);
+    Log::d(1, 3, 4, 6, -3, 12.345, "are you ok?", 34.23); // Bug: -34.23
     std::cout << buf[ridx];
     return 0;
 }
